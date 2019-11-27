dfacad3e1e9f39230b1094a4cb2413ae8da9688e
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-27 14:54:06 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,3 @@
+// Provate a riprodurre uno slider di immagini simile a quello visto a lezione, con grafica totalmente a piacere!
+// BONUS: cambiare immagine cliccando sui pallini (mostrando quindi l'immagine con indice corrispondente)
+// Nome repo: js-jq-carousel
